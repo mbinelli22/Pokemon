36966fdb4af5babdf5e17b79cce78a6ad1bb0424
mbinelli22 <mbinelli1996@gmail.com>
2019-10-11 19:26:26 -0500

    outline for Charmander class

--- src/HW4/Charmander.java
+++ src/HW4/Charmander.java
@@ -1,7 +1,28 @@
 package HW4;
 
-public class Charmander {
+public class Charmander extends Pokemon{
 
-	//TODO
+	private static final int fireBall = 5;
+	private static final int bite = 4;
+	
+	public Charmander(int health, int power) {
+		
+	}
+	
+	public boolean specialAttack(Pokemon target) {
+		
+	}
+	
+	public boolean physicalAttack(Pokemon target) {
+		
+	}
+	
+	public String toString() {
+		
+	}
+	
+	public boolean equals(Object other) {
+		
+	}
 
 }
