035b42712b5a697f32037ca389ecf2807ef78494
mbinelli22 <mbinelli1996@gmail.com>
2019-10-11 19:23:04 -0500

    outline for Pikachu class added

--- src/HW4/Pikachu.java
+++ src/HW4/Pikachu.java
@@ -2,6 +2,27 @@ package HW4;
 
 public class Pikachu extends Pokemon{
 
-	//TODO
+	private static final int thunderBolt = 6;
+	private static final int swipe = 3;
+	
+	public Pikachu(int health, int power) {
+		
+	}
+	
+	public boolean specialAttack(Pokemon target) {
+		
+	}
+	
+	public boolean physicalAttack(Pokemon target) {
+		
+	}
+	
+	public String toString() {
+		
+	}
+	
+	public boolean equals(Object other) {
+		
+	}
 
 }
