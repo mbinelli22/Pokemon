649d486d586ab554bc3ac023a39b342d888a144e
mbinelli22 <mbinelli1996@gmail.com>
2019-10-11 19:29:19 -0500

    outline for Vulpix class

--- src/HW4/Vulpix.java
+++ src/HW4/Vulpix.java
@@ -1,7 +1,28 @@
 package HW4;
 
-public class Vulpix {
+public class Vulpix extends Pokemon{
 
-	//TODO
+	private static final int flashFire = 7;
+	private static final int swipe = 2;
+	
+	public Vulpix(int health, int power) {
+		
+	}
+	
+	public boolean specialAttack(Pokemon target) {
+		
+	}
+	
+	public boolean physicalAttack(Pokemon target) {
+		
+	}
+	
+	public String toString() {
+		
+	}
+	
+	public boolean equals(Object other) {
+		
+	}
 	
 }
