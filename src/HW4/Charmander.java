d3eedc3d7540d81936434d88765a1e6462fd6aa5
mbinelli22 <mbinelli1996@gmail.com>
2019-10-22 12:43:25 -0500

    specialAttact() & physicalAttack() in Charmander class updated

--- src/HW4/Charmander.java
+++ src/HW4/Charmander.java
@@ -6,15 +6,35 @@ public class Charmander extends Pokemon{
 	private static final int bite = 4;
 	
 	public Charmander(int health, int power) {
-		
+		super("Charmander", health, power);
 	}
 	
+	@Override
 	public boolean specialAttack(Pokemon target) {
-		
+		if(this.isDefeated() || target.isDefeated()) {
+			return false;
+		}
+		if(this.currentPower > 0) {
+			if(this.currentPower >= fireBall) {
+				target.hurt(fireBall);
+				this.currentPower -= fireBall;
+			}
+			else {
+				target.hurt(this.currentPower);
+				this.currentPower = 0;
+			}
+			return true;
+		}
+		return false;
 	}
 	
+	@Override
 	public boolean physicalAttack(Pokemon target) {
-		
+		if(this.isDefeated() || target.isDefeated()) {
+			return false;
+		}
+		target.hurt(bite);
+		return true;
 	}
 	
 	public String toString() {
