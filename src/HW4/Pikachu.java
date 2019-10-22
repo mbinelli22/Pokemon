d6ebc11e13d2ed1dc581ae1ce311eb094c1035bb
mbinelli22 <mbinelli1996@gmail.com>
2019-10-22 12:52:36 -0500

    physicalAttack() & specialAttack() in Vulpix & Pikachu class updated

--- src/HW4/Pikachu.java
+++ src/HW4/Pikachu.java
@@ -6,15 +6,35 @@ public class Pikachu extends Pokemon{
 	private static final int swipe = 3;
 	
 	public Pikachu(int health, int power) {
-		
+		super("Pikachu", health, power);
 	}
 	
+	@Override
 	public boolean specialAttack(Pokemon target) {
-		
+		if(this.isDefeated() || target.isDefeated()) {
+			return false;
+		}
+		if(this.currentPower > 0) {
+			if(this.currentPower >= thunderBolt) {
+				target.hurt(thunderBolt);
+				this.currentPower -= thunderBolt;
+			}
+			else {
+				target.hurt(this.currentPower);
+				this.currentPower = 0;
+			}
+			return true;
+		}
+		return false;
 	}
 	
+	@Override
 	public boolean physicalAttack(Pokemon target) {
-		
+		if(this.isDefeated() || target.isDefeated()) {
+			return false;
+		}
+		target.hurt(swipe);
+		return true;
 	}
 	
 	public String toString() {
--- src/HW4/Vulpix.java
+++ src/HW4/Vulpix.java
@@ -6,15 +6,35 @@ public class Vulpix extends Pokemon{
 	private static final int swipe = 2;
 	
 	public Vulpix(int health, int power) {
-		
+		super("Vulpix", health, power);
 	}
 	
 	public boolean specialAttack(Pokemon target) {
-		
+		if(this.isDefeated() || target.isDefeated()){
+			return false;
+		}
+
+		if(this.currentPower>0){
+			if(this.currentPower>=flashFire) {
+				target.hurt(flashFire);
+				this.currentPower -= flashFire;
+			} else {
+				target.hurt(this.currentPower);
+				this.currentPower=0;
+			}
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	public boolean physicalAttack(Pokemon target) {
-		
+		if(this.isDefeated() || target.isDefeated())
+			return false;
+
+		target.hurt(swipe);
+		return true;
+
 	}
 	
 	public String toString() {
